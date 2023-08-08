97d1f8e3f7377c6057a0dc3ad5ef2a0a781dd708
matibien <139499739+matibien@users.noreply.github.com>
2023-08-08 17:44:12 -0300

    piano

--- www/js/script.js
+++ www/js/script.js
@@ -1,10 +1,15 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
   volumeSlider = document.querySelector(".volume-slider input"),
-  keysCheckbox = document.querySelector(".keys-checkbox input");
+  keysCheckbox = document.querySelector(".keys-checkbox input"),
+  notesDisplay = document.querySelector(".notes-display .notes");
 
 let allKeys = [],
-  audio,
-  activeNotes = new Set(); // Usamos un Set para almacenar las notas activas
+  audio = new Audio(`../tunes/a.wav`), // by default, audio src is "a" tune
+  activeNotes = new Set();
+
+const updateNotesDisplay = () => {
+  notesDisplay.textContent = [...activeNotes].join(", ");
+};
 
 const playTune = (notes) => {
   // Detener la reproducción de las notas activas
@@ -17,27 +22,39 @@ const playTune = (notes) => {
   // Crear una nueva instancia de Audio para cada nota y reproducirlas todas
   notes.forEach((note) => {
     const audioNote = new Audio(`../tunes/${note}.wav`);
+    audioNote.volume = volumeSlider.value; // Aplicar el volumen del slider
     audioNote.play();
   });
 
   // Agregar todas las notas al conjunto de notas activas
-  notes.forEach((note) => activeNotes.add(note));
+  activeNotes = new Set(notes);
 
-  // Remover las notas activas después de un tiempo
-  setTimeout(() => {
-    notes.forEach((note) => activeNotes.delete(note));
-  }, 150);
+  // Actualizar el contenido del div de notas para mostrar las notas activas
+  updateNotesDisplay();
+
+  // Mostrar la nota en el teclado
+  notes.forEach((note) => {
+    const keyElement = document.querySelector(`[data-key="${note}"]`);
+    showNoteOnKeyboard(keyElement);
+  });
 };
 
 pianoKeys.forEach((key) => {
   allKeys.push(key.dataset.key);
-  key.addEventListener("click", () => playTune([key.dataset.key])); // Ahora pasamos un array con la nota al hacer clic
+  key.addEventListener("click", () => playTune([...activeNotes, key.dataset.key]));
 });
 
 const handleVolume = (e) => {
-  if (audio) {
-    audio.volume = e.target.value;
-  }
+  const newVolume = e.target.value;
+  activeNotes.forEach((note) => {
+    const audioNote = new Audio(`tunes/${note}.wav`);
+    audioNote.volume = newVolume; // Aplicar el nuevo volumen a las notas activas
+    if (!audioNote.paused) {
+      audioNote.pause();
+      audioNote.currentTime = 0;
+      audioNote.play();
+    }
+  });
 };
 
 const showHideKeys = () => {
@@ -47,19 +64,34 @@ const showHideKeys = () => {
 const pressedKey = (e) => {
   const key = e.key.toLowerCase();
   if (allKeys.includes(key) && !activeNotes.has(key)) {
-    // Reproducir el acorde solo si la nota no está en el conjunto de notas activas
-    playTune([...activeNotes, key]); // Ahora pasamos un array con todas las notas activas y la nueva nota
+    playTune([...activeNotes, key]);
   }
 };
 
 const releasedKey = (e) => {
   const key = e.key.toLowerCase();
   if (allKeys.includes(key)) {
-    activeNotes.delete(key); // Eliminamos la nota del conjunto de notas activas al soltar la tecla
+    activeNotes.delete(key);
+    updateNotesDisplay();
+
+    // Ocultar la nota en el teclado al soltar la tecla
+    const keyElement = document.querySelector(`[data-key="${key}"]`);
+    hideNoteOnKeyboard(keyElement);
   }
 };
 
 keysCheckbox.addEventListener("click", showHideKeys);
+keysCheckbox.addEventListener("change", () => {
+  if (keysCheckbox.checked) {
+    // Si la casilla está marcada, mostrar todas las teclas
+    pianoKeys.forEach((key) => key.classList.remove("hide"));
+  } else {
+    // Si la casilla no está marcada, ocultar todas las teclas
+    pianoKeys.forEach((key) => key.classList.add("hide"));
+  }
+  
+});
+
 volumeSlider.addEventListener("input", handleVolume);
 document.addEventListener("keydown", pressedKey);
 document.addEventListener("keyup", releasedKey);
