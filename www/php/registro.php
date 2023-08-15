55a05c14a15548288522d237070fc097b8668913
ilangar <110467573+ilangar@users.noreply.github.com>
2023-08-15 11:31:56 -0300

    .

--- www/php/registro.php
+++ www/php/registro.php
@@ -1,22 +1,28 @@
 
 <?php
-
+    $hostname = $_ENV['aws.connect.psdb.cloud'];
+    $dbName = $_ENV['manbra'];
+    $username = $_ENV['d7r7ihtxp2q5di8xlmop'];
+    $password = $_ENV['pscale_pw_CjAGcVTOqgxwpDN5mIs5Cf2yhXoaqUzAcQG0HA5HF3s'];
+    
     $mysqli = mysqli_init();
-    $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-    $mysqli->real_connect($_ENV["aws.connect.psdb.cloud"], $_ENV["d7r7ihtxp2q5di8xlmop"], $_ENV["pscale_pw_CjAGcVTOqgxwpDN5mIs5Cf2yhXoaqUzAcQG0HA5HF3s"], $_ENV["manbra"]);
+    $mysqli->ssl_set(NULL, NULL, '/etc/ssl/certs/ca-certificates.crt', NULL, NULL);
+    $mysqli->real_connect($hostname, $username, $password, $dbName, $port);
     
     if ($mysqli->connect_error) 
     {
         die("Connection failed: " . $mysqli->connect_error);   
     }
+    else 
+    {
+        die("Connected successfully");
+    }
     
-    // Obtener los datos del formulario
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
-    // Insertar los datos en la base de datos
     $sql = "INSERT INTO Usuarios (nombre, apellido, usuario, contrasena) VALUES ('$nombre', '$apellido', '$usuario', '$contrasena')";
     
     echo $sql;
