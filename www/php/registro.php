ffef2864b00c6c1941ed5d1643c4eed73350b640
ilangar <110467573+ilangar@users.noreply.github.com>
2023-08-22 09:48:21 -0300

    .

--- www/php/registro.php
+++ www/php/registro.php
@@ -2,8 +2,7 @@
     $mysqli = mysqli_init();
     $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
     $mysqli->real_connect($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
-    $mysqli->close();
-  
+   
     if ($mysqli->connect_error)
     {
         die("Connection failed: " . $mysqli->connect_error);
