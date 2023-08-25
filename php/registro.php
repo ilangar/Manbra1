2333fa6065f9aff7d7683847c22cd46faa8d10db
ilangar <110467573+ilangar@users.noreply.github.com>
2023-08-25 09:33:28 -0300

    certificado db

--- php/registro.php
+++ php/registro.php
@@ -1,6 +1,6 @@
 <?php
     $mysqli = mysqli_init();
-    $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+    $mysqli->ssl_set(NULL, NULL, "../etc/ssl/certs/ca-certificates.crt", NULL, NULL);
     $mysqli->real_connect($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
    
     if ($mysqli->connect_error)
