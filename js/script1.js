835ead4e99804bff84dda4d639abbdbf73f1d5f6
MatiasBieniawski <matibieniawski@gmail.com>
2023-08-28 12:03:18 -0300

    avance

--- js/script1.js
+++ js/script1.js
@@ -4,12 +4,64 @@ const resetButton = document.getElementById("reset-button");
 const options = document.getElementsByClassName("option");
 
 const instruments = [
-  "../instrumentos/guitarra acustica.wav",
-  "../instrumentos/guitarra.mp4",
-  "../instrumentos/bateria.mp3",
-  "../instrumentos/bajo.wav",
-  "../instrumentos/acordeon.mp3"
+  "../instrumentos/cuerdas/guitarra acustica.wav",
+  "../instrumentos/cuerdas/guitarra.mp4",
+  "../instrumentos/cuerdas/violin.mp3",
+  "../instrumentos/cuerdas/bajo.wav"
 ];
+
+const images = [
+  "../instrumentos/cuerdas/guitarra acustica.webp",
+  "../instrumentos/cuerdas/guitarra.webp",
+  "../instrumentos/cuerdas/guitarra acustica.webp",
+  "../instrumentos/cuerdas/bajo.png"
+];
+
+
+const container = document.querySelector("#instrumentos");
+
+const img1 = document.createElement("img");
+img1.src = "../instrumentos/cuerdas/guitarra acustica.webp";
+img1.alt = "guitarra";
+img1.class="option";
+img1.width="150";
+img1.height="150";
+
+
+const img2 = document.createElement("img");
+img2.src = "../instrumentos/cuerdas/guitarra.webp";
+img2.alt = "guitarra";
+img2.class="option";
+img2.width="150";
+img2.height="150";
+
+const img3 = document.createElement("img");
+img3.src = "../instrumentos/cuerdas/violin.png";
+img3.alt = "violin";
+img3.class="option";
+img3.width="150";
+img3.height="150";
+
+const img4 = document.createElement("img");
+img4.src = "../instrumentos/cuerdas/bajo.png";
+img4.alt = "bajo";
+img4.class="option";
+img4.width="150";
+img4.height="150";
+
+
+container.appendChild(img1);
+container.appendChild(img2);
+container.appendChild(img3);
+container.appendChild(img4);
+
+
+/*
+      <img src="../instrumentos/cuerdas/guitarra acustica.webp" class="option" data-index="0">
+      <img src="../instrumentos/cuerdas/guitarra.webp" class="option" data-index="1">
+      <img src="../instrumentos/cuerdas/violin.png" class="option" data-index="2">
+      <img src="../instrumentos/cuerdas/bajo.png" class="option" data-index="3">
+*/
 
 let correctInstrumentIndex = Math.floor(Math.random() * instruments.length);
 
@@ -25,20 +77,8 @@ for (let i = 0; i < options.length; i++) {
 function playSound() {
   audioPlayer.src = instruments[correctInstrumentIndex];
   audioPlayer.play();
-}
 
-function checkAnswer(selectedIndex) {
-  if (selectedIndex === correctInstrumentIndex) {
-    alert("¡Correcto! Adivinaste el instrumento correcto.");
-  } else {
-    alert("¡Oops! Intenta de nuevo.");
-  }
 }
-
-function resetGame() {
-  correctInstrumentIndex = -1; // Restablecemos el índice correcto
-}
-
 // Configuración de los índices correctos según la ruta de la pista de audio
 if (audioPlayer.src === instruments[0]) {
   correctInstrumentIndex = 0;
@@ -49,3 +89,23 @@ if (audioPlayer.src === instruments[0]) {
 } else if (audioPlayer.src === instruments[3]) {
   correctInstrumentIndex = 3;
 }
+else if (audioPlayer.src === instruments[4]) {
+  correctInstrumentIndex = 4;
+}
+
+function checkAnswer(selectedIndex) {
+  if (selectedIndex === correctInstrumentIndex) {
+    alert("¡Correcto! Adivinaste el instrumento correcto.");
+    showNextInstruments();
+  } else {
+    alert("¡Oops! Intenta de nuevo.");
+  }
+}
+
+function resetGame() {
+  correctInstrumentIndex = Math.floor(Math.random() * instruments.length);
+  audioPlayer.pause();
+  audioPlayer.currentTime = 0;
+}
+
+
