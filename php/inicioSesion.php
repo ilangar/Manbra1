4def4464fc01199dd39446ec3aec3aee1303d48e
ilangar <110467573+ilangar@users.noreply.github.com>
2023-08-28 11:57:17 -0300

    inicio sesion y registro casi terminado

---
+++ php/inicioSesion.php
@@ -0,0 +1,31 @@
+<?php
+    $_ENV = parse_ini_file(".env");
+    $mysqli = mysqli_init();
+    $mysqli->ssl_set(NULL, NULL, "../cacert.pem", NULL, NULL);
+    $mysqli->real_connect($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+
+    if ($mysqli->connect_error)  
+    {
+        die("Conexión fallida: " . $mysqli->connect_error);
+    }
+
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+
+    $sql = "SELECT * FROM Usuarios WHERE usuario = '$usuario' AND contrasena = '$contrasena'";
+    $result = $mysqli->query($sql);
+
+    if ($result->num_rows == 1) 
+    {
+        $result->free_result();
+        header("Location: ../html/index.html");
+        exit();
+    } 
+    else 
+    {
+        $result->free_result();
+        echo "Usuario o contraseña incorrectos";
+    }
+
+    $mysqli->close();
+?>
