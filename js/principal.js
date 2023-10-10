ee300ddcf980f93429584fefc40d86e363b94a2a
MatiasBieniawski <matibieniawski@gmail.com>
2023-10-10 10:39:04 -0300

    avance reconocer y principal 10/10

---
+++ js/principal.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAbrirVentana = document.getElementById("btnAjustes");
+  
+    btnAbrirVentana.addEventListener("click", function () {
+      // Abre una nueva ventana o pestaña del navegador
+      window.open('../diseño/FondoReconocer.png', "menuAjustes.png", "opciones");
+    });
+  });
+  
