f5e78c5305e6eebe2ea9cbf25403eeecb70ed820
MatiasBieniawski <matibieniawski@gmail.com>
2023-11-06 12:03:12 -0300

    musicograma

--- php/musicograma.php
+++ php/musicograma.php
@@ -5,12 +5,18 @@ session_start();
 <html>
 <head>
     <style>
+        
+    body {
+    background-image: url('../diseño/fondomusicograma.png');
+    background-size: cover;
+}
         .image-container {
             display: flex;
             flex-wrap: wrap;
             justify-content: center; /* Centra horizontalmente el contenido */
             align-items: flex-end; /* Coloca el contenido en la parte inferior */
-            height: 80vh; /* Altura del 80% del viewport height (altura de la ventana) */
+            height: 50vh; /* Altura del 80% del viewport height (altura de la ventana) */
+            background-color: #f0f0f0; /* Fondo gris claro para el contenedor */
         }
 
         .image {
@@ -30,44 +36,69 @@ session_start();
         h1 {
             text-align: center;
         }
+
+        .image-drop-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center; /* Centra horizontalmente el contenido */
+            align-items: flex-start; /* Coloca el contenido en la parte superior */
+            height: 20vh; /* Altura del 20% del viewport height (altura de la ventana) */
+        }
+
+        .image-drop-container .image {
+            max-width: 100px;
+            max-height: 100px;
+        }
     </style>
 </head>
 <body>
     <h1>Generador de Patrones de Imágenes</h1>
     
-    <div class="image-container" id="pattern-container"></div>
+    <div class="image-container" id="image-container">
+        <!-- Deja este espacio vacío para las imágenes iniciales -->
+    </div>
+    
+    <div class="image-drop-container" id="image-drop-container"></div>
 
     <script>
-        const images = ["../diseño/hamburguesa.png","../diseño/platillos.png","../diseño/silencio.png", "../diseño/chasquido.png", "../diseño/aplauso.png", "../diseño/bongo.png", "../diseño/xilofono.png"]; // Rutas de las imágenes
-        const patternContainer = document.getElementById('pattern-container');
+        const images = ["../diseño/hamburguesa.png", "../diseño/platillos.png", "../diseño/silencio.png", "../diseño/chasquido.png", "../diseño/aplauso.png", "../diseño/bongo.png", "../diseño/xilofono.png"]; // Rutas de las imágenes
+        const imageContainer = document.getElementById('image-container');
+        const imageDropContainer = document.getElementById('image-drop-container');
+        const maxImages = 6; // Número máximo de imágenes en el contenedor de abajo
 
-        // Agrega las imágenes al contenedor
+        // Agrega las imágenes iniciales al contenedor superior
         images.forEach((imageSrc, index) => {
+            const image = createImageElement(imageSrc);
+            imageContainer.appendChild(image);
+        });
+
+        // Función para crear un elemento de imagen
+        function createImageElement(imageSrc) {
             const image = document.createElement('img');
             image.src = imageSrc;
             image.className = 'image';
             image.draggable = true;
             image.setAttribute('ondragstart', 'drag(event)');
-            patternContainer.appendChild(image);
-        });
+            return image;
+        }
 
         // Función para permitir el arrastre de las imágenes
         function drag(event) {
             event.dataTransfer.setData('image', event.target.src);
         }
 
-        // Manejar el soltar de la imagen en el contenedor
-        patternContainer.ondragover = (event) => event.preventDefault();
+        // Manejar el soltar de la imagen en el contenedor inferior
+        imageDropContainer.ondragover = (event) => event.preventDefault();
 
-        patternContainer.ondrop = (event) => {
+        imageDropContainer.ondrop = (event) => {
             event.preventDefault();
-            const imageSrc = event.dataTransfer.getData('image');
-            const image = document.createElement('img');
-            image.src = imageSrc;
-            image.className = 'image';
-            image.draggable = true;
-            image.setAttribute('ondragstart', 'drag(event)');
-            patternContainer.appendChild(image);
+            if (imageDropContainer.childElementCount < maxImages) {
+                const imageSrc = event.dataTransfer.getData('image');
+                if (imageSrc) {
+                    const image = createImageElement(imageSrc);
+                    imageDropContainer.appendChild(image);
+                }
+            }
         };
     </script>
 </body>
